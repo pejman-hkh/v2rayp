5c122b546acf2b4901fc3982a7a404922d60e123
pejman hkh <pejman.hkh@gmail.com>
2026-06-23 15:28:46 +0330

    fix: modify xray path

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -63,7 +63,7 @@ fn spawn_v2ray(process: &mut Option<Child>, config_path: String) -> String {
     }
 
     let v2ray_path = match std::env::consts::OS {
-        "linux" => "bin/xray",
+        "linux" => "/usr/bin/xray",
         "windows" => "C:\\Program Files\\V2Ray\\v2ray.exe",
         "macos" => "/usr/local/bin/v2ray",
         _ => return format!("unsupported OS: {}", std::env::consts::OS),
